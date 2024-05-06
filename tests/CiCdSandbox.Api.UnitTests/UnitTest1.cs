b00ee5250de50b16e602bc9bb17c6596e296f634
LRharlekin <13718192+LRharlekin@users.noreply.github.com>
2024-05-06 16:41:11 +0300

    testing branch initial commit

--- tests/CiCdSandbox.Api.UnitTests/UnitTest1.cs
+++ tests/CiCdSandbox.Api.UnitTests/UnitTest1.cs
@@ -1,10 +1,19 @@
+using Moq;
+using FluentAssertions;
+
 namespace CiCdSandbox.Api.UnitTests;
 
 public class UnitTest1
 {
+    // Arrange
+
+    // Act
+
+    // Assert
+
     [Fact]
     public void Test1()
     {
-
+        Console.WriteLine("Hello World! (Testing)");
     }
 }
